67e772f9f445a14de1135c863f387b935df1a715
BeaPucheWork <bpuche.work@gmail.com>
2025-07-28 15:49:27 +0200

    Update sales_daily: Purchase_cost replaced by Margin

--- sales_daily.sql
+++ sales_daily.sql
@@ -1,5 +1,7 @@
-SELECT date_date,
-ROUND (SUM (turnover),2) AS turnover,
-COUNT (orders_id) AS nb_sales
+SELECT 
+  date_date
+ , ROUND (SUM (turnover),2) AS turnover
+ , COUNT (orders_id) AS nb_sales
+ , ROUND((turnover - purchase_cost ),2) AS margin
 FROM `data-analytics-bootcamp-363212.course14.gwz_sales`
 GROUP BY date_date
